e2ce9d2ebfb2a8a62a5b56cc8b4595e3c4c645ba
satinder <satinder042890@gmail.com>
2018-11-16 14:28:06 -0700

    complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,11 @@
 var wins=0;
-var countries=["india","usa","france","italy","england","canada"];
+var countries=["india","usa","france","italy","england","canada","australia","newzealand","germany","japan"];
 var no_Of_Guesses=10;
 var letters_Guessed=[];
 var computer=null;
 var user_Guess=null;
 var correct=[];
+
 //computer randomly pick a word from array of strings
 
 function comp_Words()
@@ -31,6 +32,9 @@ function letters_Guessed_push()
     document.querySelector("#guessedLetter").innerHTML=letters_Guessed.join(", ");
 
 }
+
+//correct letter insert at its exact location
+
 function correct_Words_Push()
 {
     console.log("correct words push function execute");
@@ -51,6 +55,8 @@ function clearAll()
     comp_Words();
  
 }
+
+//when computer selects random word, it will display dashes on the screen
 
 function dashes()
 {
@@ -108,14 +114,69 @@ function compare()
             wins++;
             console.log(wins);
             document.querySelector("#win").innerHTML=wins;
+            
+            $("#pic").attr("src",function()
+            {
+                var src= "assets/images/usa.jpg";
+                if(computer === "india")
+                {
+                    src= "assets/images/indiaf.jpg";
+                }
+                if(computer === "usa")
+                {
+                    src= "assets/images/usaf.jpg";
+                }
+                if(computer === "france")
+                {
+                    src= "assets/images/france.jpg";
+                }
+                if(computer === "canada")
+                {
+                    src= "assets/images/canada.jpg";
+                }
+                if(computer === "italy")
+                {
+                    src= "assets/images/italy.jpg";
+                }
+                if(computer === "england")
+                {
+                    src= "assets/images/england.jpg";
+                }
+                if(computer === "australia")
+                {
+                    src= "assets/images/australia.jpg";
+                }
+                if(computer === "newzealand")
+                {
+                    src= "assets/images/newzealand.jpg";
+                }
+                if(computer === "germany")
+                {
+                    src= "assets/images/germany.jpg";
+                }
+                if(computer === "japan")
+                {
+                    src= "assets/images/japan.jpg";
+                }
+                if(computer === "france")
+                {
+                    src= "assets/images/france.jpg";
+                }
+                return src;
+            })
             console.log("you won");
-            alert("congratulations you find the word");
-            clearAll();
+            //document.querySelector("#right").innerHTML=computer;
+             //alert("congratulations you find the word");
+             if(confirm("you find this word.. Do you want to continue with another word?"))
+             {
+                clearAll();
+             }
+           
         }
     };   
 }
 
-//executes everytime user press any key
+//executes when user press any key to start the game
 
 document.onkeypress=function(e)
 {
