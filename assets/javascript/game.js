56acd9f3152a179ad3e1bd28c42505da39978381
satinder <satinder042890@gmail.com>
2018-11-20 18:34:51 -0700

    added https to jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -197,7 +197,7 @@ document.onkeypress=function(e)
 {
        var user_Guess1=String.fromCharCode(e.which).toLowerCase();
        audioElement = document.createElement("audio");
-       audioElement.setAttribute("src", "http://soundimage.org/wp-content/uploads/2017/11/Bounce-Light-2.mp3");
+       audioElement.setAttribute("src", "https://soundimage.org/wp-content/uploads/2017/11/Bounce-Light-2.mp3");
        console.log(user_Guess1);
        comp_Words();
        compare();
